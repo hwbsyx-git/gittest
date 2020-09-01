e0d39d5117ab27df1a4b8616bba8cece57790248
HWB <a644757092@qq.com>
2020-09-01 18:58:29 +0800

    新增一个类

---
+++ src/main/java/test/Hello.java
@@ -0,0 +1,11 @@
+package test;
+
+/**
+ * @author He Wenbo
+ * @create 2020-09-01 18:57
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("更新git");
+    }
+}
